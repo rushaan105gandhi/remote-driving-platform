eebb2d02739ba032455fc0de294ef73c3cebe694
rushaan gandhi <rushaansps@gmail.com>
2023-10-02 18:39:01 +0530

    Converting OpenCV frame for sending over the network

--- src/video_capture.cpp
+++ src/video_capture.cpp
@@ -1,15 +1,52 @@
-// video_capture.cpp
 #include "video_capture.h"
 #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <winsock2.h>
 
-void sendFrameToRemoteOperator(const cv::Mat& frame) {
-    // Simulating sending the processed frame to the remote operator over the network
-    
-    std::cout << "Sending processed frame to remote operator...\n";
+#pragma comment(lib, "ws2_32.lib") // Link against the Winsock2 library
+
+void sendFrameOverNetwork(const cv::Mat& frame) {
+    // Initialize Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize Winsock\n";
+        return;
+    }
+
+    // Create a socket and establish a connection to the server
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Error creating socket\n";
+        WSACleanup();
+        return;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(12345); // Replace with the server's port
+    serverAddress.sin_addr.s_addr = inet_addr("your_server_ip_here"); // Replace with the server's IP address
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "Error connecting to the server\n";
+        closesocket(clientSocket);
+        WSACleanup();
+        return;
+    }
+
+    // Convert the OpenCV frame to a format suitable for sending over the network
+    std::vector<uchar> encodedFrame;
+    cv::imencode(".jpg", frame, encodedFrame);
+
+    // Send the frame data
+    send(clientSocket, reinterpret_cast<char*>(encodedFrame.data()), encodedFrame.size(), 0);
+
+    // Close the socket when done
+    closesocket(clientSocket);
+    WSACleanup();
 }
 
 void captureVideoFeed() {
-    cv::VideoCapture capture(0); 
+    cv::VideoCapture capture(0);
     if (!capture.isOpened()) {
         std::cerr << "Failed to open camera\n";
         return;
@@ -17,24 +54,22 @@ void captureVideoFeed() {
 
     cv::Mat frame;
     while (true) {
-        capture >> frame; // Capturing a frame
+        capture >> frame;
         if (frame.empty()) {
             break;
         }
-        
-        
-        cv::Mat processedFrame;
-        cv::cvtColor(frame, processedFrame, cv::COLOR_BGR2GRAY);
 
-        // Sending the processed frame to a remote operator (simulated)
-        sendFrameToRemoteOperator(processedFrame);
+        // Process the frame if needed (e.g., image processing)
 
-        // Exit when 'Esc' is pressed
+        // Send the frame over the network
+        sendFrameOverNetwork(frame);
+
+        // Display the frame (for testing purposes)
+        cv::imshow("Video Feed", frame);
         if (cv::waitKey(1) == 27) {
             break;
         }
     }
 
     capture.release();
-    cv::destroyAllWindows();
 }
